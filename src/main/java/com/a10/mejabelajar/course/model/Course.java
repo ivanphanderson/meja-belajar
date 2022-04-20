737f2619a8df5746d94eeaedcf0c3da9c5d6f6bd
Ivan Phanderson <ivan.phanderson@ui.ac.id>
2022-04-20 22:40:49 +0700

    Resolve error

--- src/main/java/com/a10/mejabelajar/course/model/Course.java
+++ src/main/java/com/a10/mejabelajar/course/model/Course.java
@@ -1,4 +1,21 @@
 package com.a10.mejabelajar.course.model;
 
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+@Data
+@NoArgsConstructor
 public class Course {
+
+    @Id
+    @Column(name = "course_id")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy= "org.hibernate.id.UUIDGenerator")
+    private String id;
 }

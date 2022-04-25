73afc809f455aed81cd07ef732f209a16d7d19c0
Ivan Phanderson <ivan.phanderson@ui.ac.id>
2022-04-25 22:30:34 +0700

    Modify teacher repo, model, and service

--- src/main/java/com/a10/mejabelajar/auth/repository/TeacherRepository.java
+++ src/main/java/com/a10/mejabelajar/auth/repository/TeacherRepository.java
@@ -1,9 +1,11 @@
 package com.a10.mejabelajar.auth.repository;
 
 import com.a10.mejabelajar.auth.model.Teacher;
+import com.a10.mejabelajar.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+    Teacher findByUser(User user);
 }
--- src/main/java/com/a10/mejabelajar/auth/service/TeacherService.java
+++ src/main/java/com/a10/mejabelajar/auth/service/TeacherService.java
@@ -1,9 +1,12 @@
 package com.a10.mejabelajar.auth.service;
 
 import com.a10.mejabelajar.auth.model.Teacher;
+import com.a10.mejabelajar.auth.model.User;
 
 import java.util.List;
 
 public interface TeacherService {
     List<Teacher> getTeachers();
+
+    Teacher getTeacherByUser(User user);
 }

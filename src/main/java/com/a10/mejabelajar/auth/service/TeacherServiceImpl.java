277642505d911c4f439fe49e6f367547cc8530ad
MARKUS LEONARD WIJAYA <markus.leonard@ui.ac.id>
2022-04-25 21:55:41 +0700

    add services

---
+++ src/main/java/com/a10/mejabelajar/auth/service/TeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.a10.mejabelajar.auth.service;
+
+import com.a10.mejabelajar.auth.model.Teacher;
+import com.a10.mejabelajar.auth.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeacherServiceImpl implements TeacherService{
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    @Override
+    public List<Teacher> getTeachers() {
+        return teacherRepository.findAll();
+    }
+}

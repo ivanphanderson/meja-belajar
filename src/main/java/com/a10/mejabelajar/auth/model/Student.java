4a10536c8dbba2180dba13c707be5ab0338d7797
Ivan Phanderson <ivan.phanderson@ui.ac.id>
2022-05-16 16:22:08 +0700

    Try to add observer

--- src/main/java/com/a10/mejabelajar/auth/model/Student.java
+++ src/main/java/com/a10/mejabelajar/auth/model/Student.java
@@ -1,12 +1,14 @@
 package com.a10.mejabelajar.auth.model;
 
 import com.a10.mejabelajar.admin.model.Log;
+import com.a10.mejabelajar.course.model.CourseNotification;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -14,9 +16,12 @@ import java.util.List;
 @Table(name = "student")
 public class Student {
     @Id
-    @GeneratedValue(generator="uuid2")
-    @GenericGenerator(name="uuid2", strategy= "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
+
+    @Column(name = "lastNotifBtnClick")
+    private Date lastNotifBtnClick;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
@@ -24,4 +29,7 @@ public class Student {
 
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private List<Log> log;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<CourseNotification> courseNotifications;
 }

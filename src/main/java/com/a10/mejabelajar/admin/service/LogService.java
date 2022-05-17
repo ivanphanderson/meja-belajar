d7a327d10e53d03b0c66d9ce08fe2d34fa7cbec7
MARKUS LEONARD WIJAYA <markus.leonard@ui.ac.id>
2022-05-18 00:54:23 +0700

    add exception and errorPage

--- src/main/java/com/a10/mejabelajar/admin/service/LogService.java
+++ src/main/java/com/a10/mejabelajar/admin/service/LogService.java
@@ -4,7 +4,9 @@ import com.a10.mejabelajar.admin.model.Log;
 import com.a10.mejabelajar.auth.model.Student;
 import com.a10.mejabelajar.auth.model.Teacher;
 import com.a10.mejabelajar.auth.model.User;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface LogService {
@@ -12,7 +14,8 @@ public interface LogService {
 
     void deleteLog(String id);
 
-    Log createLog(double hour, String desc, Student student, Teacher teacher);
+    Log createLog(String starts, String ends, String duration, String desc, Student student, Teacher teacher);
+    String countDuration(String start, String end);
 
     List<Log> getAll();
 
